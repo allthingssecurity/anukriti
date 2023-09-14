2d2df877ec2f4d9156938bc6c9f7949391aea905
Jain <shashank.jain01@sap.com>
2023-09-14 06:01:36 +0530

    my first cut of anukriti

--- anukriti.py
+++ anukriti.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import insightface
 import os
+import torch
 from insightface.app import FaceAnalysis
 from moviepy.editor import *
 
@@ -33,27 +34,33 @@ def process_frame(frame, frame_count, output_folder, app, swapper, source_face, 
     frame_filename = os.path.join(output_folder, f"frame_{frame_count}.png")
     cv2.imwrite(frame_filename, frame)
 
-def read_and_process_video(video_path, output_folder, app, swapper, source_face, frame_photo_face_features):
+def read_and_process_video(video_path, output_folder, app, swapper, source_face_dict):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print("Error: Could not open video.")
         return
 
     frame_count = 0
-    frames = []
-    with ThreadPoolExecutor() as executor:
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Video has ended.")
-                break
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Video has ended.")
+            break
 
-            executor.submit(process_frame, frame, frame_count, output_folder, app, swapper, source_face, frame_photo_face_features)
-            frame_count += 1
+        faces = app.get(frame)
+        for face in faces:
+            face_features = face.normed_embedding
+            for frame_photo_name, (source_face, frame_photo_face_features) in source_face_dict.items():
+                similarity = np.dot(face_features, frame_photo_face_features.T)
+                if similarity > 0.5:
+                    frame = swapper.get(frame, face, source_face[0], paste_back=True)
+
+        frame_filename = os.path.join(output_folder, f"frame_{frame_count}.png")
+        cv2.imwrite(frame_filename, frame)
+        frame_count += 1
 
     cap.release()
     cv2.destroyAllWindows()
-
 def reassemble_video(output_folder, original_video_path, final_output_folder):
     # Get frame rate of original video
     cap = cv2.VideoCapture(original_video_path)
@@ -105,28 +112,45 @@ def reassemble_video(output_folder, original_video_path, final_output_folder):
 
     print("Video reassembled successfully with audio.")
 
-
+def clear_gpu_memory(app, swapper):
+    del app
+    del swapper
+    torch.cuda.empty_cache()
 
 def main():
     create_directories()
 
     app, swapper = initialize_insightface('/content/drive/MyDrive/inswapper_128 (1).onnx')
 
-    source_img = cv2.imread('/content/drive/MyDrive/test/source.jpg')
-    source_face = app.get(source_img)
+    # Dictionary to hold multiple source and frame photos
+    source_face_dict = {}
 
-    frame_photo = cv2.imread('/content/drive/MyDrive/test/asrani.jpg')
-    frame_photo_face = app.get(frame_photo)
-    frame_photo_face_features = frame_photo_face[0].normed_embedding
+    # Example with two source-frame photo pairs
+    source_img1 = cv2.imread('/content/drive/MyDrive/khujli/ji_1.jpg')
+    source_face1 = app.get(source_img1)
+    frame_photo1 = cv2.imread('/content/drive/MyDrive/khujli/amol.jpg')
+    frame_photo_face1 = app.get(frame_photo1)
+    frame_photo_face_features1 = frame_photo_face1[0].normed_embedding
+    source_face_dict["frame1"] = (source_face1, frame_photo_face_features1)
+
+    source_img2 = cv2.imread('/content/drive/MyDrive/khujli/kat.jpg')
+    source_face2 = app.get(source_img2)
+    frame_photo2 = cv2.imread('/content/drive/MyDrive/khujli/actress.jpg')
+    frame_photo_face2 = app.get(frame_photo2)
+    frame_photo_face_features2 = frame_photo_face2[0].normed_embedding
+    source_face_dict["frame2"] = (source_face2, frame_photo_face_features2)
 
     output_folder = '/content/frames'
-    video_path = '/content/drive/MyDrive/test/asrani.mp4'
+    video_path = '/content/drive/MyDrive/khujli/amol1.mp4'
 
-    read_and_process_video(video_path, output_folder, app, swapper, source_face, frame_photo_face_features)
+    read_and_process_video(video_path, output_folder, app, swapper, source_face_dict)
 
-    original_video_path = '/content/drive/MyDrive/test/asrani.mp4'
-    final_output_folder='/content/output'
-    reassemble_video(output_folder, original_video_path,final_output_folder)
+    original_video_path = '/content/drive/MyDrive/khujli/amol1.mp4'
+    final_output_folder = '/content/output'
+    reassemble_video(output_folder, original_video_path, final_output_folder)
+
+    # Remove model from GPU
+    clear_gpu_memory(app, swapper)
 
 if __name__ == "__main__":
     main()
